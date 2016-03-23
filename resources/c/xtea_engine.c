8dd9ed22596babe1c73b3d28a8a9087d91232afb
Derek Chan <derekchan1994@gmail.com>
2016-03-23 16:21:09 -0700

    working xtea_engine

--- resources/c/xtea_engine.c
+++ resources/c/xtea_engine.c
@@ -12,10 +12,14 @@
 #define KEY_SCHEDULE_CONSTRAINT 0x9e3779b9
 #define NUM_ROUNDS	32
 
-static void xtea_engine_encipher(uint32_t data[2], uint32_t const key[4]);
-static void xtea_engine_decipher(uint32_t data[2], uint32_t const key[4]);
+static void xtea_engine_encipher(uint32_t data[2], const uint32_t key[4]);
+static void xtea_engine_decipher(uint32_t data[2], const uint32_t key[4]);
+static void xtea_engine_encipher_str(const char * data, int length, char ** output, int * output_length, const uint32_t key[4]);
+static void xtea_engine_decipher_str(const char * data, int length, char ** output, int * output_length, const uint32_t key[4]);
+static int xtea_engine_char_array_to_xtea_array(const char * chars, const int char_length, uint32_t ** uint32s, int * uint32_length);
+static int xtea_engine_xtea_array_to_char_array(const uint32_t * uint32ts, const int uint32_length, char ** chars, int * char_length);
 
-static void xtea_engine_encipher(uint32_t data[2], uint32_t const key[4]) {
+static void xtea_engine_encipher(uint32_t data[2], const uint32_t key[4]) {
 	uint32_t d0 = data[0];
 	uint32_t d1 = data[1];
 	uint32_t delta = KEY_SCHEDULE_CONSTRAINT;
@@ -32,7 +36,7 @@ static void xtea_engine_encipher(uint32_t data[2], uint32_t const key[4]) {
 	data[1] = d1;
 }
 
-static void xtea_engine_decipher(uint32_t data[2], uint32_t const key[4]) {
+static void xtea_engine_decipher(uint32_t data[2], const uint32_t key[4]) {
 	uint32_t d0 = data[0];
 	uint32_t d1 = data[1];
 	uint32_t delta = KEY_SCHEDULE_CONSTRAINT;
@@ -49,82 +53,93 @@ static void xtea_engine_decipher(uint32_t data[2], uint32_t const key[4]) {
 	data[1] = d1;
 }
 
-static void xtea_engine_encipher_str(char * data, int length, uint32_t const key[4]) {
+static void xtea_engine_encipher_str(const char * data, int length, char ** output, int * output_length, const uint32_t key[4]) {
 	int i;
+	uint32_t * temp;
+	int templength;
 
-	/* We have to convert our char * into a uint32_t * */
-	int data_num_bits = sizeof(char) * length;
-	printf("length: %d\n", length);
-	printf("data_num_bits: %d\n", data_num_bits);
-
-	/* xtea's block size is 64 bits (2 * 32-bit uints) */
-	int num_blocks = 1 + (data_num_bits / 64);
-	printf("num_blocks: %d\n", num_blocks);
-
-	/* We need an even number of blocks */
-	if (num_blocks % 2 == 0)
-		num_blocks++;
-
-	/* Create a buffer to hold our data */
-	char * buf = calloc(num_blocks * 2, sizeof(uint32_t));
-	printf("calloc: %d, %lu\n", num_blocks * 2, sizeof(uint32_t));
-
-	/* Copy in our data */
-	for (i = 0; i < length; i++)
-		buf[i] = data[i];
+	xtea_engine_char_array_to_xtea_array(data, length, &temp, &templength);
 
 	/* Encipher */
-	//for (i = 0; i < num_blocks; i++)
-		xtea_engine_encipher(((uint32_t *)buf), key);
+	for (i = 0; i < templength; i += 2)
+		xtea_engine_encipher(&temp[i], key);
 
-	/* Copy out our data */
-	for (i = 0; i < length; i++)
-		data[i] = buf[i];
+	xtea_engine_xtea_array_to_char_array(temp, templength, output, output_length);
 
-	free(buf);
+	free(temp);
 }
 
-static void xtea_engine_decipher_str(char * data, int length, uint32_t const key[4]) {
-int i;
+static void xtea_engine_decipher_str(const char * data, int length, char ** output, int * output_length, const uint32_t key[4]) {
+	int i;
+	uint32_t * temp;
+	int templength;
 
-	/* We have to convert our char * into a uint32_t * */
-	int data_num_bits = sizeof(char) * length;
-	printf("length: %d\n", length);
-	printf("data_num_bits: %d\n", data_num_bits);
-
-	/* xtea's block size is 64 bits (2 * 32-bit uints) */
-	int num_blocks = 1 + (data_num_bits / 64);
-	printf("num_blocks: %d\n", num_blocks);
-
-	/* We need an even number of blocks */
-	if (num_blocks % 2 == 0)
-		num_blocks++;
-
-	/* Create a buffer to hold our data */
-	char * buf = calloc(num_blocks * 2, sizeof(uint32_t));
-	printf("calloc: %d, %lu\n", num_blocks * 2, sizeof(uint32_t));
-
-	/* Copy in our data */
-	for (i = 0; i < length; i++)
-		buf[i] = data[i];
+	xtea_engine_char_array_to_xtea_array(data, length, &temp, &templength);
 
 	/* Decipher */
-	//for (i = 0; i < num_blocks; i++)
-		xtea_engine_decipher(((uint32_t *)buf), key);
+	for (i = 0; i < templength; i+= 2)
+		xtea_engine_decipher(&temp[i], key);
 
-	/* Copy out our data */
-	for (i = 0; i < length; i++)
-		data[i] = buf[i];
-
-	free(buf);
+	xtea_engine_xtea_array_to_char_array(temp, templength, output, output_length);
+	
+	free(temp);
 }
 
-void test(uint32_t data1, uint32_t data2) {
+static int xtea_engine_char_array_to_xtea_array(const char * chars, const int char_length, uint32_t ** uint32s, int * uint32_length) {
+	char * padded_char_buffer;
+	
+	/**
+	 * Our XTEA array needs block sizes of 64 bits 
+	 * 64 bits  = 8 chars
+	 */
+	int new_char_length = char_length + (8 - char_length % 8);
+	//printf("new_char_length: %d\n", new_char_length);
+
+	padded_char_buffer = malloc(sizeof(char) * new_char_length);
+	if (padded_char_buffer == NULL) {
+		printf("Out of memory!\n");
+		return 1;		
+	}
+	memset(padded_char_buffer, '\0', new_char_length);
+
+	/* Copy chars into our padded buffer. Remaining space will be filled with \0 */
+	memcpy(padded_char_buffer, chars, sizeof(char) * char_length);
+
+	/* HACKY: cast our padded buffer into a uint32_t buffer. The sizes should match because of math */
+	*uint32s = (uint32_t *)padded_char_buffer;
+	*uint32_length = (new_char_length / 4);
+
+	return 0;
+}
+
+static int xtea_engine_xtea_array_to_char_array(const uint32_t * uint32ts, const int uint32_length, char ** chars, int * char_length) {
+	char * output;
+	int output_length;
+	
+	/* There are 4 chars in a uint32_t */
+	output_length = uint32_length * 4;
+	output = malloc(sizeof(char) * output_length);
+	if (output == NULL) {
+		printf("Out of memory!\n");
+		return 1;
+	}
+	
+	memcpy(output, uint32ts, sizeof(char) * output_length);
+
+	*chars = output;
+	*char_length = output_length;
+
+	return 0;
+}
+
+void test1(uint32_t data1, uint32_t data2) {
 	uint32_t key[4] = {1,2,3,4};
 
 	uint32_t buf[2];
 	buf[0] = data1;
 	buf[1] = data2;
+
+	printf("[%s] <%d> <%d>\n", __func__, data1, data2);
 
 	printf("Testing enciphering %d %d\n", buf[0], buf[1]);
 
@@ -137,26 +152,82 @@ void test(uint32_t data1, uint32_t data2) {
 	printf("Done deciphering %d %d\n", buf[0], buf[1]);	
 }
 
+void test2(const char * str) {
+	uint32_t * xt;
+	int xt_len;
+	char * str2;
+	int str2_len;
+
+	printf("\n%s: <%s>\n", __func__, str);
+
+	xtea_engine_char_array_to_xtea_array(str, strlen(str) + 1, &xt, &xt_len);
+	xtea_engine_xtea_array_to_char_array(xt, xt_len, &str2, &str2_len);
+
+	if (strcmp(str, str2) == 0)
+		printf("PASSED\n");
+	else
+		printf("FAILED\n");
+	printf("\n");
+
+	free(xt);
+	free(str2);
+}
+
+void test3(const char * str) {
+	uint32_t key[4] = {1,2,3,4};
+
+	char * enciphered;
+	int enciphered_length;
+	char * deciphered;
+	int deciphered_length;
+
+	printf("\n%s: <%s>\n", __func__, str);
+
+	xtea_engine_encipher_str(str, strlen(str), &enciphered, &enciphered_length, key);
+
+	printf("%s\n", strcmp(str, enciphered) == 0 ? "FAILED" : "PASSED");
+
+	xtea_engine_decipher_str(enciphered, enciphered_length, &deciphered, &deciphered_length, key);
+	
+	printf("%s\n", strcmp(str, deciphered) == 0 ? "PASSED" : "FAILED");
+
+	free(enciphered);
+	free(deciphered);
+}
+
 int main(int argc, char ** argv) {
 	uint32_t d1 = 3;
 	uint32_t d2 = 26;
-	char str[16];
-	uint32_t key[4] = {1,2,3,4};
-	sprintf(str, "hello");
 
 	if (argc == 3) {
 		d1 = atoi(argv[1]);
 		d2 = atoi(argv[2]);
 	}
 
-	test(d1, d2);
+	test1(d1, d2);
+	test1(0x00000000, 0x00000000);
+	test1(0xFFFFFFFF, 0x00000000);
+	test1(0x00000000, 0xFFFFFFFF);
+	test1(0xFFFFFFFF, 0xFFFFFFFF);
 
-	printf("\n");
+	test2("");
+	test2("a");
+	test2("asdf");
+	test2("1234");
+	test2("asdf1234");
+	test2("1234asdf");
+	test2("1234asdf5678jkl;");
 
-	printf("str: %s\n", str);
-	xtea_engine_encipher_str(str, strlen(str), key);
-	printf("str: %s\n", str);
-	xtea_engine_decipher_str(str, strlen(str), key);
-	printf("str: %s\n", str);
+	test3("");
+	test3("a");
+	test3("asdf");
+	test3("1234");
+	test3("asdf1234");
+	test3("1234asdf");
+	test3("1234asdf5678jkl;");
+
+	test3("some random message");
+	test3("Hello World!\nWhat if i have escapes too?");
+
 	return 0;
 }
